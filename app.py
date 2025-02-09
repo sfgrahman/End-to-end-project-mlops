dcf39f452f50aef4d84aafdc8553058153e595d4
sfgrahman <sfgrahman35@gmail.com>
2025-02-09 09:54:06 +0600

    Custom exception testing

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from src.logger import logging
+from src.exception import CustomException
+import os, sys
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET","POST"])
 def index():
-    logging.info("Testing logging method in the pipelines")
-    return "welcome to machine learning end to end project pipeline-Explore MLOPs"
+    try:
+        raise Exception("Testing custom exception")
+    except Exception as e:
+        abc =CustomException(e,sys)
+        logging.info(abc.error_message)
+        return "welcome to machine learning end to end project pipeline-Explore MLOPs"
 
 if __name__=="__main__":
     app.run(debug=True)
